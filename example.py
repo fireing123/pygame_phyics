4858f850023934c5bd2f4f44d33c163b2de50ff2
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-09-15 20:34:32 +0900

    c

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ pygame_engine.init((1000, 800), "example", "D:/pygame_engine/example/objects/")
 @pygame_engine.world("D:/pygame_engine/example/example.json")
 def start():
     pygame_engine.scene.brightening()
-    
+    pygame_engine.screen.fill((25,255,255))
     pygame_engine.game_loop()
     
 start()
