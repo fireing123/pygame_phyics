714873635342f248112a5855700395a46e079d85
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-09-19 18:31:11 +0900

    last

--- pygame_phyics/__init__.py
+++ pygame_phyics/__init__.py
@@ -1,6 +1,13 @@
+
+__version__ = "0.1"
+
 PPM =20
+
 from pygame_phyics.game import Game
 from pygame_phyics.object import GameObject
 
 from pygame_phyics.object import StaticObject
 from pygame_phyics.object import DynamicObject
+from pygame_phyics.object import ImageObject
+
+from pygame_phyics.sheet import SurfaceSheet
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pygame_engine',
+    name='pygame_phyics',
     version='0.1',
     author= 'fireing123',
     author_email= 'gimd82368@gmail.com',
-    url= 'https://github.com/fireing123/pygame_engine',
-    install_requires=["pygame"],
+    url= 'https://github.com/fireing123/pygame_phyics',
+    install_requires=["pygame", "box2d-py"],
     packages=find_packages(),
 )
