47b4cd6643306311d84465f61fdb2721e4a485b2
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-10-02 20:02:49 +0900

    pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygame_phyics',
-    version='0.1',
+    version='0.2',
     author= 'fireing123',
     author_email= 'gimd82368@gmail.com',
     url= 'https://github.com/fireing123/pygame_phyics',
-    install_requires=["pygame", "box2d-py", "hangul_utils"],
+    install_requires=["pygame", "box2d-py"],
     packages=find_packages(),
 )
