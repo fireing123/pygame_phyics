cdef7a9bf11843657f7b18ff793aecfe098278e4
fireing123 <88523379+fireing123@users.noreply.github.com>
2023-10-25 15:03:48 +0900

    pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygame_phyics',
-    version='0.3.1',
+    version='0.4.0',
     author= 'fireing123',
     author_email= 'gimd82368@gmail.com',
     url= 'https://github.com/fireing123/pygame_phyics',
